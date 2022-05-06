c06ef7b2abcf26f3ceb819289f5a77155dd7c74d
ulises <ulisesromanmartinez101098@gmail.com>
2022-05-06 18:39:43 -0500

    primeros comandos en git

---
+++ js/datatables.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#commands').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ comandos",
+            "zeroRecords": "No se encontraron comandos con los datos solicitados",
+            "info": "Mostrando del _PAGE_ al _PAGES_ de un total de _MAX_ comandos",
+            "infoEmpty": "No hay comandos encontrados",
+            "infoFiltered": "(_MAX_ comandos consultados)",
+            "search": "Buscar: ",
+            "paginate": {
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        }
+    });
+});
